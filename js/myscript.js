99cc2e9241112f252146d6ff136d531039d8bbfe
devinz1 <devinzohner@gmail.com>
2014-09-26 17:53:34 -0600

    commented out my hide button codenothing happens with my other code index.html stays unchanged

--- js/myscript.js
+++ js/myscript.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
    //('.button').on('click', function(e){
    	//console.log(e);
    	//debugger;
-   	$( ".button" ).hide();
-   	$('#char-count ').hide();
+   	//$( ".button" ).hide();
+   //	$('#char-count ').hide();
    		
- });
+ //});
    
-	//$('.tweet-content').click(function()){
-		//$(this).animate({
-			//"width" : '500px',
-			//"height" : '60px'
-		//}, 1000, function() {
+	$('.tweet-content').click(function()){
+		$(this).animate({
+			"width" : '500px',
+			"height" : '60px'
+		}, 1000, function() {
 
-		//});
- 	//});
+		});
+ 	});
   
    //});
 
